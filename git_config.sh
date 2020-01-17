ee160625b8c5ee9ef08df8d688536d983d73145b
whwu <908393142@qq.com>
2020-01-17 16:52:03 +0800

    Add git difftool setting

--- git_config.sh
+++ git_config.sh
@@ -13,3 +13,15 @@ git config --global --replace-all user.name whwu
 git config --global --replace-all core.filemode false  #忽略文件权限的变化
 git config --global --replace-all core.autocrlf true  #Git可以在你提交时自动地把行结束符CRLF转换成LF，而在签出代码时把LF转换成CRLF。
 git config --global --replace-all core.whitespace cr-at-eol #忽略换行符的差异
+
+
+
+#设置git代码对比工具
+git config --global diff.tool bc4
+git config --global difftool.prompt false
+#git config --global difftool.bc4.cmd '"D:\InstalledSoftware\CodeManage\Beyond Compare 4\BCompare.exe" "$LOCAL" "$REMOTE"'
+
+git config --global merge.tool bc4
+git config --global mergetool.prompt false
+#git config --global mergetool.bc4.cmd '"D:\InstalledSoftware\CodeManage\Beyond Compare 4\BCompare.exe" "$LOCAL" "$REMOTE" "$BASE" "$MERGED"'
+git config --global mergetool.bc4.trustexitcode true
